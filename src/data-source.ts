19e762b569c110264548e215b5135d30ccd7ec0e
Maksym Boiko <maksym782@gmail.com>
2025-03-27 03:30:56 +0100

    fixed data-source

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,22 +1,27 @@
 import * as dotenv from 'dotenv';
 import { DataSource } from 'typeorm';
+import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions';
 import { User } from './user/entities/user.entity';
 
-const commonOptions = {
+const commonOptions: PostgresConnectionOptions = {
   type: 'postgres',
   entities: [User],
   host: process.env.POSTGRES_HOST,
   port: Number.parseInt(process.env.POSTGRES_PORT as unknown as string),
 };
+interface iDbOptions {
+  development: PostgresConnectionOptions;
+  production: PostgresConnectionOptions;
+}
 
-const dbOptions = {
+const dbOptions: iDbOptions = {
   development: {
     ...commonOptions,
     username: process.env.POSTGRES_USER,
     password: process.env.POSTGRES_PASSWORD,
     database: process.env.POSTGRES_DB,
     synchronize: true,
-  }),
+  },
   production: {
     ...commonOptions,
     username: process.env.POSTGRES_USER,
