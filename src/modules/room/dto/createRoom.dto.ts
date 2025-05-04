ac72e9923e488ee784f61011afc0b643c0fc7dbe
Makrootx <maksym782@gmail.com>
2025-05-05 00:26:10 +0200

    Added tournament connection to room

--- src/modules/room/dto/createRoom.dto.ts
+++ src/modules/room/dto/createRoom.dto.ts
@@ -1,7 +1,11 @@
+import { GenerateTournamentDto } from 'src/modules/tournament/dto/createTournament.dto';
 import { RoomUser } from '../../roomUser/entities/roomUser.entity';
+import { UserIdentifierType } from 'src/modules/user/entities/user.entity';
+import { CreateRoomUserDto } from 'src/modules/roomUser/dto/createRoomUser.dto';
 
 export class CreateRoomDto {
   readonly name: string;
   readonly icon?: Buffer;
-  readonly roomUsers?: RoomUser[];
+  readonly roomUsers?: CreateRoomUserDto[]
+  readonly tournament?: GenerateTournamentDto;
 }
